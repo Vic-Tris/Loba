1c9c2ef45dfa5a43a8a5c4d211f0de004141d21f
Vic-Tris <y2jvictorious12@gmail.com>
2026-05-18 14:01:06 +0100

    text color

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -37,10 +37,10 @@ export function Hero() {
             
             <h1 className="text-4xl lg:text-[2.85rem] font-heading font-extrabold text-primary leading-tight mb-6">
               Get Your Academic Work Done Right; <br className="hidden md:block"/> 
-              <span className="text-slate-800 ">Without Stress, Delays, or Rewrites</span>
+              <span className="text-[#0056A6] ">Without Stress, Delays, or Rewrites</span>
             </h1>
             
-            <p className="text-lg text-slate-800 mb-10 max-w-2xl leading-relaxed">
+            <p className="text-lg text-[#0056A6] mb-10 max-w-2xl leading-relaxed">
               We help students and professionals deliver high-quality projects, proposals, and career documents with expert precision and fast turnaround.
             </p>
 
